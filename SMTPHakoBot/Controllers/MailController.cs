9c71a79d75fe19a4a576b1546c048e64172f6b6a
Simon Dusausoy <dusausoy.simon@gmail.com>
2019-03-21 02:21:28 +0100

    Creation du SMTP

---
+++ SMTPHakoBot/Controllers/MailController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SMTPHakoBot.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Mail")]
+    public class MailController : Controller
+    {
+        private SmtpClient smtpClient;
+        public MailController(SmtpClient smtpClient)
+        {
+            this.smtpClient = smtpClient;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            await smtpClient.SendMailAsync(new MailMessage(
+                from : "hakobot27@gmail.com",
+                to: "hakobot27@gmail.com",
+                subject: "Test message subject",
+                body: "Test message body"
+                ));
+
+            return Ok("OK");
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            smtpClient.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SMTPHakoBot/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace SMTPHakoBot
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+ 
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<SmtpClient>((serviceProvider) =>
+            {
+                var config = serviceProvider.GetRequiredService<IConfiguration>();
+                return new SmtpClient()
+                {
+                    Host = config.GetValue<String>("Email:Smtp:Host"),
+                    Port = config.GetValue<int>("Email:Smtp:Port"),
+                    Credentials = new NetworkCredential(
+                                            config.GetValue<String>("Email:Smtp:Username"),
+                                            config.GetValue<String>("Email:Smtp:Password")
+                                        )
+                };
+            });
+            services.AddMvc();
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
